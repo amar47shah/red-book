44b75a2f0bb5a25f97aec7d414c6181e5563de8b
Amar Shah <amar47shah@gmail.com>
2017-05-18 14:36:51 +0200

    Add `fib` (2.1)

--- 2/MyModule.scala
+++ 2/MyModule.scala
@@ -15,6 +15,15 @@ object MyModule {
     go(n, 1)
   }
 
+  def fib(n: Int): Int = {
+    def go(n: Int, acc: Int, prev: Int): Int =
+      if (n < 1) 0
+      else if  (n == 1) acc
+           else go(n-1, acc+prev, acc)
+
+    go(n, 1, 0)
+  }
+
   private def format(s: String, f: Function[Int, Int], x: Int): String = {
     val msg = "The %s, of %d is %d"
     msg.format(s, x, f(x))
